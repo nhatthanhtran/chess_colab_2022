488f830abf3052d5a8a121b46ec2606df62035ea
Thanh <binnhatthanhtran@gmail.com>
2022-09-25 23:22:41 -0700

    Update the piece and pawn class

--- src/Piece/pawn.py
+++ src/Piece/pawn.py
@@ -3,10 +3,32 @@
 Pawn Class
 """
 from piece import Piece
+import numpy as np
 
 
 class Pawn(Piece):
-    strName = "Pawn"
+    def __init__(self, strColor, intCurXPos, intCurYPos):
+        super().__init__(strColor, intCurXPos, intCurYPos)
+        self.blnMoved = False
 
-    def setColor(self, strColor):
-        strColor = strColor
+    def arrPossibleMoves(self):
+        if self.strColor == "BLACK":
+            intMoveDir = -1
+        else:
+            intMoveDir = 1
+
+        if (self.blnMoved):
+            return(np.array([self.intCurXPos, self.intCurYPos+intMoveDir]))
+        else:
+            arrPosMoves = np.zeros((2, 2), dtype=np.intc)
+            arrPosMoves[0, :] = np.array(
+                [self.intCurXPos, self.intCurYPos+intMoveDir])
+            arrPosMoves[1, :] = np.array(
+                [self.intCurXPos, self.intCurYPos+intMoveDir*2])
+
+            return(arrPosMoves)
+
+    def Move(self, intNewXPos, intNewYPos):
+        self.intCurXPos = intNewXPos
+        self.intCurYPos = intNewYPos
+        self.blnMoved = True
