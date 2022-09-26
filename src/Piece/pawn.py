2fc37601dfd280661be69215324178dcc47aa309
Thanh <binnhatthanhtran@gmail.com>
2022-09-25 17:20:27 -0700

    Add new pawn class

---
+++ src/Piece/pawn.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Pawn Class
+"""
+from piece import Piece
+
+
+class Pawn(Piece):
+    strName = "Pawn"
+
+    def setColor(self, strColor):
+        strColor = strColor
