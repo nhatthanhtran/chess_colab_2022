d30d42d6ef01612af98aae96c6849cd126e652a5
John Palacios <johnpalacios@dhcp-v016-241.mobile.uci.edu>
2022-09-30 20:24:07 -0700

    First commit for John with skeloton Board and Space classes.

---
+++ src/Board/space.py
@@ -0,0 +1,11 @@
+""Space class
+""
+
+class Space:
+    def __init__(self, color):
+        self.str_color = color
+        self.bln_occupied = false
+
+    def occupy(piece):
+        if self.bln_occupied
+            self.pc_piece.capture()
