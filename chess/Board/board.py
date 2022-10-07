32c7b79c0f5b0f227b8d349819446d4d7e04c783
John Palacios <sn0wf0xzer0@gmail.com>
2022-10-07 15:37:44 -0700

    rudamentary board, space, and testharness complete.

--- chess/Board/board.py
+++ chess/Board/board.py
@@ -9,9 +9,13 @@ INT_BOARDSIZE = 8 # Gloablal constant board side length in spaces
 """
 
 class Board:
+    _board = [[None for _ in range(INT_BOARDSIZE)] for _ in range(INT_BOARDSIZE)] # Will hold spaces
     def __init__(self):
-        _strColor + ("white" if (i + j) % 2 == 0 else "black")
-        board = [[Space()]] # Need to create 2d array, DO NOT RUN BROKEN
         for i in range(INT_BOARDSIZE):
             for j in range(INT_BOARDSIZE):
-                self.board[i][j] = Space(_strColor) # Set alternating space color
+                str_color_ = "white" if (i + j) % 2 == 0 else "black"
+                self._board[i][j] = Space(str_color_)
+
+    @classmethod
+    def get_spaceColor(self, i, j):
+        return self._board[i][j].str_color
---
+++ chess/Board/boardTest.py
@@ -0,0 +1,10 @@
+"""Testing board object in this file. Author: John Palacios
+"""
+
+from board import Board
+
+"""Testing Space colors
+"""
+
+test = Board()
+print(test.get_spaceColor(0, 0))
