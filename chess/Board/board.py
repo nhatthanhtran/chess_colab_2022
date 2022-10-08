9bf897c43be6d66480e66f779e44f782c39f335a
John Palacios <sn0wf0xzer0@gmail.com>
2022-10-07 21:46:17 -0700

    trying to fix import accross folders error

--- chess/Board/board.py
+++ chess/Board/board.py
@@ -3,7 +3,7 @@
 
 from space import Space
 
-INT_BOARDSIZE = 8 # Gloablal constant board side length in spaces
+_INT_BOARDSIZE = 8 # Gloablal constant board side length in spaces
 
 """Assuming upper left corner is board[0][0] and white
 """
@@ -16,6 +16,16 @@ class Board:
                 str_color_ = "white" if (i + j) % 2 == 0 else "black"
                 self._board[i][j] = Space(str_color_)
 
+    # returns space color string
     @classmethod
     def get_spaceColor(self, i, j):
         return self._board[i][j].str_color
+
+    # returns true of piece at (s_x, s_y) can be moved to (d_x, d_y)
+    @classmethod
+    def validate_move(s_x, s_y, d_x, d_y):
+        _piece = self_board[s_x][s_y].get_piece() # Do not use, under construction
+        _piece_type = type(_piece)
+        
+        # Testing type function
+        return _piece_type
--- chess/Board/space.py
+++ chess/Board/space.py
@@ -1,11 +1,30 @@
 """Space class
 """
 
+from pathlib import Path
+from os import chdir
+init_path = Path(__file__)
+chdir('..')
+chdir('Piece')
+print(Path.cwd())
+from pawn import Pawn
+
 class Space:
     def __init__(self, color):
         self.str_color = color
         self.bln_occupied = False
 
+    @classmethod
     def occupy(piece):
         if self.bln_occupied:
             self.pc_piece.capture()
+            self.pc_piece = piece
+        else:
+             self.pc_piece = piece
+
+    @classmethod
+    def get_piece(self):
+        if self.bln_occupied:
+            return self.pc_piece
+        else:
+            return None
