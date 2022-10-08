b07a7ef9c3f4176d3a5ee1c00f7477cc8d5f4944
Thanh <binnhatthanhtran@gmail.com>
2022-10-08 13:25:19 -0700

    change the test to import pawn

--- chess/Board/boardTest.py
+++ chess/Board/boardTest.py
@@ -2,9 +2,13 @@
 """
 
 from chess.Board.board import Board
-
+from chess.Piece.pawn import Pawn
 """Testing Space colors
 """
 
-test = Board()
-print(test.get_spaceColor(0, 0))
+# test = Board()
+# print(test.get_spaceColor(0, 0))
+
+test2 = Pawn("black", 1, 1, 1)
+
+print(test2.possible_moves())
