32a9ffafc9e7c3c57d62370c5395169e939733eb
Thanh <binnhatthanhtran@gmail.com>
2022-10-28 00:16:00 -0700

    Update game to be smarter, rather than hard code specific player

--- chess/Game/game.py
+++ chess/Game/game.py
@@ -7,51 +7,51 @@ from chess.Player.player import Player
 
 class Game():
     def __init__(self):
-        self.plr_white = Player("A", "white")
-        self.plr_black = Player("B", "black")
+        plr_white = Player("A", "white")
+        plr_black = Player("B", "black")
+        self.lst_players = [plr_white, plr_black]
         self.brd_board = Board()
         self.bln_end = False
+        self.bln_whos_turn = True # Base on RBG: False (0) is black True (1) is white
+        self.int_turn_counter = 1
     def play(self):
         while not self.bln_end:
-            # white move
-            int_white_cur_x, int_white_cur_y = 0, 0
-            int_white_nxt_x, int_white_nxt_y = 1, 1
+
+            # Get user input
+            int_cur_x, int_cur_y, int_nxt_x, int_nxt_y = self._get_player_move()
             self._update_player(
-                self.plr_white,
-                int_white_cur_x,
-                int_white_cur_y,
-                int_white_nxt_x,
-                int_white_nxt_y,
+                self.lst_players[self.bln_whos_turn],
+                int_cur_x,
+                int_cur_y,
+                int_nxt_x,
+                int_nxt_y,
             )
+
+            # draw the current board
+            self._draw_board()
 
             # check win condition
             if self.brd_board.locked():
                 self._game_end()
                 break
-
-
+            
+            # update turn
+            self._update_turn()
 
             
-            
-            # black move
-            int_black_cur_x, int_black_cur_y = 0, 0
-            int_black_nxt_x, int_black_nxt_y = 1, 1
-            self._update_player(
-                self.plr_black,
-                int_black_cur_x,
-                int_black_cur_y,
-                int_black_nxt_x,
-                int_black_nxt_y,
-            )
-            # check win condition
-            if self.brd_board.locked():
-                self._game_end()
 
     def _game_end(self):
         self.bln_end = True
 
     def game_save(self):
         pass
+
+    def _update_turn(self):
+        self.bln_whos_turn = not self.bln_whos_turn
+        self.int_turn_counter += 1
+
+    def _draw_board(self):
+        self.brd_board.draw()
 
     def _update_player(self, plr_player: Player, int_cur_x, int_cur_y, int_nxt_x, int_nxt_y):
         
@@ -62,8 +62,16 @@ class Game():
 
         if self.brd_board.get_piece(int_nxt_x, int_nxt_y):
             plr_player.capture_piece(self.brd_board.get_piece(int_nxt_x, int_nxt_y))
-        
+    
 
+    def _get_player_move(self):
+
+        # Wait for user inputs from UI here
+
+        if self.bln_whos_turn: # white
+            return 0, 0, 1, 1
+        else: # black
+            return 7, 7, 7, 6
 
     
 
