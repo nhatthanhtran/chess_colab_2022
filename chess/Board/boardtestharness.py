1432c6a36c0029e15bab9bfae1059a6c0cfce1cc
John Palacios <sn0wf0xzer0@gmail.com>
2022-11-04 21:30:09 -0700

    fixed issue with pawn move validation

--- chess/Board/boardtestharness.py
+++ chess/Board/boardtestharness.py
@@ -1,9 +1,7 @@
 # This file is to test board set up and functionality during initial development only.
 # Author: John Palacios
 
-from nis import match
-from operator import truediv
-from unittest import case
+
 from chess.Board.board import Board
 from chess.Piece.bishop import Bishop
 from chess.Piece.king import King
@@ -17,4 +15,13 @@ from chess.Board.space import Space
 test1 = Board()
 
 print(test1.get_spaceColor(0, 0))
+print(test1.get_board_state())
+test1.move_piece(1, 6, 1, 4)
+test1.move_piece(1, 4, 1, 3)
+test1.move_piece(1, 3, 1, 2)
+print(test1.get_board_state())
+piece = test1.move_piece(0, 1, 1, 2)
+print("captured: ", piece.bln_captured)
+print(piece.int_cur_x_pos)
+print(piece.int_cur_y_pos)
 print(test1.get_board_state())
